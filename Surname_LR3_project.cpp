6266314f3b9f58e9e1e40ddf4a6cd352351aa3af
Books-Master18 <osipovasasha11@yandex.ru>
2025-03-29 16:13:31 +0300

    fun 2

--- Surname_LR3_project.cpp
+++ Surname_LR3_project.cpp
@@ -19,7 +19,10 @@ char inputFirstChar() {
 
 // Функция для ввода второго символа
 char inputSecondChar() {
-
+    char ch;
+    cout << "Введите второй символ: ";
+    cin >> ch;
+    return ch;
 }
 
 
@@ -33,3 +36,39 @@ void printAsciiSum(char ch1, char ch2) {
 
 }
 
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    char ch1, ch2;
+    int choice;
+
+    do {
+        cout << "Пожалуйста, введите номер действия или 0, если вы хотите закрыть программу:" << endl;
+        cout << "1. Ввести первый символ" << endl;
+        cout << "2. Ввести второй символ" << endl;
+        cout << "3. Вывести каждый символ и код ASCII" << endl;
+        cout << "4. Вывести сумму кодов ASCII символов ch1 и ch2" << endl;
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            ch1 = inputFirstChar();
+            break;
+        case 2:
+            ch2 = inputSecondChar();
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 0:
+            cout << "Программа завершена." << endl;
+            break;
+        default:
+            cout << "Неверный номер действия." << endl;
+        }
+        cout << endl;
+    } while (choice != 0);
+
+    return 0;
+}
